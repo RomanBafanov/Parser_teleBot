5e6278a396220ae5609d6b3a54e864bd62534c66
about_bot <kunichkin83@gmail.com>
2024-02-04 22:35:59 +0300

    pk=>fk

--- database/create_tables.py
+++ database/create_tables.py
@@ -15,17 +15,17 @@ def create_all_tables():
     cursor = connection.cursor()
 
     create_city_table_query = '''CREATE TABLE cities
-                              (ID BIGSERIAL PRIMARY KEY NOT NULL,
-                              ID_CITY INTEGER NOT NULL,
+                              (ID BIGSERIAL ,
+                              ID_CITY INTEGER PRIMARY KEY NOT NULL ,
                               CITY VARCHAR NOT NULL)
                                '''
 
     create_requests_table_query = '''CREATE TABLE requests
                                   (ID BIGSERIAL PRIMARY KEY NOT NULL,
-                                  ID_CITY INTEGER NOT NULL,
+                                  ID_CITY INTEGER NOT NULL UNIQUE,
                                   JOB_TITLE VARCHAR NOT NULL,
                                   DATE_REQUEST DATE,
-                                  FOREIGN KEY (ID_CITY) REFERENCES cities (ID))
+                                  FOREIGN KEY (ID_CITY) REFERENCES cities (ID_CITY))
                                    '''
 
     create_response_table_query = '''CREATE TABLE response
