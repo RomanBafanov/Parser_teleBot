b9cb660cd61fc3d80125408134667c674320fc66
about_bot <kunichkin83@gmail.com>
2024-02-04 16:27:02 +0300

    result print

--- handlers/default_handler/parsing.py
+++ handlers/default_handler/parsing.py
@@ -6,6 +6,9 @@ from loader import dp
 from utils.statesform import StepsForm
 from database.PARSER import get_vacancies_hh, filter_and_create_dict
 from aiogram import F
+import pandas as pd
+import openpyxl
+
 
 AREA = None
 KEYWORD = None
@@ -56,7 +59,7 @@ async def search_city_code(message: types.Message, state: FSMContext):
     )
     await message.answer(f"Введенный код города - {AREA}")
     await message.answer(
-        f' Введите название вакансии: (например, "PHP-разработчик")', reply_markup=builder.as_markup())
+        f' Введите название вакансии: (например, Python-разработчик)', reply_markup=builder.as_markup())
     await state.set_state(StepsForm.GET_VACANCY)
 
 
@@ -71,7 +74,7 @@ async def search_vacancy_name2(message: types.Message, state: FSMContext):
     builder.row(types.InlineKeyboardButton(
         text="Продолжить", callback_data="Парс")
     )
-    await message.answer(f"Введенный  - {KEYWORD}", reply_markup=builder.as_markup())
+    await message.answer(f"Введенная вакансия  - {KEYWORD}", reply_markup=builder.as_markup())
     await state.clear()
 
 
@@ -79,18 +82,30 @@ async def search_vacancy_name2(message: types.Message, state: FSMContext):
 async def get_data(callback: types.CallbackQuery):
     global KEYWORD
     global AREA
+    await callback.message.answer("processing... please wait")
     try:
         vacancies_data = get_vacancies_hh(KEYWORD, AREA)  # Передаем переменные
         result = filter_and_create_dict(vacancies_data)
         print(result)
-        for company in result:
-            print(f"**Компания:** {company['Компания']}")
-            print(f"Сайт: {company['Сайт']}")
-            if company.get('Телефон'):
-                print(f"Телефон: {company['Телефон']}")
-            print()
+        # df = pd.DataFrame([result])
+        # df.to_excel('output.xlsx', index=False)
+        # wb = openpyxl.load_workbook('output.xlsx')
+        #
+        # # Выбираем активный лист (первый лист в книге)
+        # sheet = wb.active
+        #
+        # # Устанавливаем ширину столбцов
+        # sheet.column_dimensions['A'].width = 25
+        # sheet.column_dimensions['B'].width = 20
+        # sheet.column_dimensions['C'].width = 20
+        #
+        # # Устанавливаем высоту строк
+        # sheet.row_dimensions[1].height = 15
+        # sheet.row_dimensions[2].height = 15
+        #
+        # # Сохраняем изменения в файл
+        # wb.save('output.xlsx')
 
         await callback.message.answer("Результаты выведены в терминал")
     except Exception as e:
         await callback.message.answer(f"Ошибка: {e}")
-
