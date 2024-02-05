cac4c243defd1b703828720b80b5d70e5890992d
about_bot <kunichkin83@gmail.com>
2024-02-05 08:47:21 +0300

    5 feb

--- utils/statesform.py
+++ utils/statesform.py
@@ -4,3 +4,5 @@ from aiogram.fsm.state import StatesGroup, State
 class StepsForm(StatesGroup):
     GET_VACANCY = State()
     GET_ID_CITY = State()
+    GET_CITY_ID = State()
+    GET_VACANCY_HISTORY = State()
